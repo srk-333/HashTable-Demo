b5648a1d84722827030d72cdc0bd5366f485a8af
srk-333 <sauravmk333@gmail.com>
2022-01-30 21:12:13 +0530

    [Saurav]Add:program to add element at specific index

--- HashTableDemo/Program.cs
+++ HashTableDemo/Program.cs
@@ -8,35 +8,44 @@ namespace HashTableDemo
 {
     public class Program
     {
+        static readonly MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+        /// <summary>
+        /// Perform Hash operation.
+        /// </summary>
+        public static void HashOperation()
+        {         
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "because");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "putting");
+            hash.Add("13", "themselves");
+            hash.Add("14", "deliberately");
+            hash.Add("15", "into ");
+            hash.Add("16", "paranoid");
+            hash.Add("17", "avoidable");
+            hash.Add("18", "situations");
+            string hash0 = hash.Get("0");
+            Console.WriteLine("Oth index value:" + hash0);
+            string hash3 = hash.Get("3");
+            Console.WriteLine("3rd index value:" + hash3);
+            hash.Remove(hash3);
+            string hash10 = hash.Get("10");
+            Console.WriteLine("10th index value:" + hash10);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTable Program");
-            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            CountWords(paragraph);
+            HashOperation();
             Console.ReadLine();
-        }
-        /// <summary>
-        /// Counts the words.
-        /// </summary>
-        /// <param name="paragraph">The paragraph.</param>
-        public static void CountWords(string paragraph)
-        {
-            MyMapNode<string, int> myMapNode = new MyMapNode<string, int>(6);
-            string[] words = paragraph.Split(' ');
-            foreach (string word in words)
-            {
-                if(myMapNode.Exists(word))
-                    myMapNode.Add(word.ToLower(), myMapNode.Get(word) + 1);
-                else
-                    myMapNode.Add(word.ToLower(), 1);
-            }
-            Console.WriteLine("Display after Add operation \n");
-            myMapNode.Display();
-            Console.WriteLine("\nRemove word avoidable from hash table");
-            string s = "avoidable";
-            myMapNode.Remove(s);
-            Console.WriteLine("Element in hash table after removal \n");
-            myMapNode.Display();
         }
     }
 }
